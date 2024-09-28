140773cffaea33290001465e8770694a89b76f22
Benoit2068 <88383582+Benoit2068@users.noreply.github.com>
2024-09-28 11:23:46 +0200

    Class Product + @override

--- src/Dish.java
+++ src/Dish.java
@@ -8,7 +8,7 @@ public class Dish implements Product {
         this.price = price;
         setType(type);
     }
-
+    @Override
     public void setType(String type){
         if (type.equalsIgnoreCase("starter") || type.equalsIgnoreCase("main") || type.equalsIgnoreCase("dessert")){
             this.type = type;
@@ -17,30 +17,30 @@ public class Dish implements Product {
             System.out.println("the txpe doesn't exist : Dish is not create");
         }
     }
-
+    @Override
     public void setName(String name){
         this.name = name;
     }
-
+    @Override
     public void setPrice(double price){
         this.price = price;
     }
-
+    @Override
     public String getName(){
         return name;
     }
-
+    @Override
     public double getPrice(){
         return price;
     }
-
-    public String type(){
+    @Override
+    public String getType(){
         return type;
     }
 
-
+    @Override
     public void readDish(){
-        System.out.println();
+        System.out.println("the dish is " + this.name + ", the price is " + this.price + ", the type is " + this.type +".");
     }
 
 }
