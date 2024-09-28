67296d8b39843bc757876a1a55ac0ed6cddd8e10
Benoit2068 <88383582+Benoit2068@users.noreply.github.com>
2024-09-28 10:43:14 +0200

    Class Dish + constructeur

--- src/Dish.java
+++ src/Dish.java
@@ -1,5 +1,23 @@
 public class Dish {
-    String name;
-    double price;
-    String type;
+    private String name;
+    private double price;
+    private String type;
+
+    Dish (String name, double price, String type){
+        this.name = name;
+        this.price = price;
+        setType(type);
+    }
+
+    public void setType(String type){
+        if (type.equalsIgnoreCase("starter") || type.equalsIgnoreCase("main") || type.equalsIgnoreCase("dessert")){
+            this.type = type;
+        }
+        else {
+            System.out.println("the txpe doesn't exist : Dish is not create");
+        }
+    }
+
+
+
 }
