dbb445d7acae7295c6c37f402c2575976c7b23d7
Benoit2068 <88383582+Benoit2068@users.noreply.github.com>
2024-09-29 20:40:05 +0200

    Class BaseProduct (todo : class Menu)

--- src/Dish.java
+++ src/Dish.java
@@ -1,14 +1,10 @@
-public class Dish implements Product {
-    private String name;
-    private double price;
-    private String type;
+public class Dish extends BaseProduct {
+
 
     Dish (String name, double price, String type){
-        this.name = name;
-        this.price = price;
-        setType(type);
+        super(name, price, type);
     }
-    @Override
+
     public void setType(String type){
         if (type.equalsIgnoreCase("starter") || type.equalsIgnoreCase("main") || type.equalsIgnoreCase("dessert")){
             this.type = type;
@@ -17,29 +13,10 @@ public class Dish implements Product {
             System.out.println("the txpe doesn't exist : Dish is not create");
         }
     }
-    @Override
-    public void setName(String name){
-        this.name = name;
-    }
-    @Override
-    public void setPrice(double price){
-        this.price = price;
-    }
-    @Override
-    public String getName(){
-        return name;
-    }
-    @Override
-    public double getPrice(){
-        return price;
-    }
-    @Override
-    public String getType(){
-        return type;
-    }
 
-    @Override
-    public void readDish(){
+
+
+    public void displayDish(){
         System.out.println("the dish is " + this.name + ", the price is " + this.price + ", the type is " + this.type +".");
     }
 
