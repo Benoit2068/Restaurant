732679d14e0c34818522c1057858b14dd92073ff
Benoit2068 <88383582+Benoit2068@users.noreply.github.com>
2024-09-29 21:47:50 +0200

    class menu (todo methode display menu)

--- src/Menu.java
+++ src/Menu.java
@@ -1,10 +1,38 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Menu  {
-    protected List<Product> product = new ArrayList<>();
+public class Menu extends BaseProduct {
+    protected List<Product> productsInMenu = new ArrayList<>();
+
+    Menu(Product... components) {
+        super(null, 0d, null);
+        add(components);
+    }
 
 
+    @Override
+    public void setType(String type) {
+    }
 
+    public void add(Product component){
+        productsInMenu.add(component);
+    }
+
+    public void add(Product... components){
+        productsInMenu.addAll(Arrays.asList(components));
+    }
+
+    public void remove(Product component){
+        productsInMenu.remove(component);
+    }
+
+    public void remove(Product... components){
+        productsInMenu.removeAll(Arrays.asList(components));
+    }
+
+    public void clear(){
+        productsInMenu.clear();
+    }
 
 }
--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Restaurant {
     public static void main(String[] args) {
 
         Dish salad = new Dish("salad", 4.50d, "starter");
@@ -14,7 +14,12 @@ public class Main {
         ((Drink) coca).displayDrink();
         System.out.println(coca);
 
+        Menu dishOfTheDay = new Menu();
+        dishOfTheDay.name = "table14";
+        System.out.println(dishOfTheDay);
 
+        dishOfTheDay.add(coca, salad);
+        System.out.println(dishOfTheDay.getClass().getName());
 
 
 
